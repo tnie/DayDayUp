377e4eb2360fe324006df5c6d49c8d109a8aac0b
niel <nielong2014@gmail.com>
2023-08-17 15:54:06 +0800

    QSqlQueryModel 和 QSqlTableModel

--- shijz/mainwindow.cpp
+++ shijz/mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QtDebug>
+#include <QSqlQueryModel>
+#include <QSqlTableModel>
 
 namespace  {
     QSqlError prepare()
@@ -61,7 +63,44 @@ MainWindow::MainWindow(QWidget *parent)
     {
         qDebug() << error;
     }
+    if(QSqlQueryModel *model = new QSqlQueryModel(this))
+    {
+        model->setQuery("SELECT Name, City FROM Persons");
+        if(model->lastError().isValid())
+        {
+            qDebug() << model->lastError();
+        }
+        else
+        {
+            model->setHeaderData(0, Qt::Horizontal, tr("Name"));
+            model->setHeaderData(1, Qt::Horizontal, tr("City"));
 
+            ui->tableView->setModel(model);
+            ui->tableView->show();
+        }
+    }
+    if(QSqlTableModel *model = new QSqlTableModel(this))
+    {
+        model->setTable("Persons");
+        model->setEditStrategy(QSqlTableModel::OnFieldChange);
+        if(model->select())
+        {
+            model->setHeaderData(0, Qt::Horizontal, tr("ID"));
+            model->setHeaderData(1, Qt::Horizontal, tr("Name"));
+//NOTE 只能关联整张表格，可以在 view 中隐藏不关心的列
+            ui->writableView->setModel(model);
+            ui->writableView->hideColumn(0); // don't show the ID
+            ui->writableView->show();
+            ui->writableView2->setModel(model);
+            ui->writableView2->hideColumn(0);
+            ui->writableView2->hideColumn(2);
+            ui->writableView2->show();
+        }
+        else
+        {
+            qDebug() << model->lastError();
+        }
+    }
 }
 
 MainWindow::~MainWindow()
